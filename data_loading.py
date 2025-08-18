ee88e9840163e7322181ce127217257db5fccf78
Sam Crowley <samxcrowley@gmail.com>
2025-08-18 21:33:41 +1000

    Added function to load all images from training data in a single tensor

--- data_loading.py
+++ data_loading.py
@@ -1,77 +1,35 @@
 import json
 import numpy as np
+import pandas as pd
 
 import torch
 from torch.utils.data import DataLoader, Dataset
 
-def build_e_theta_axes(points):
+def get_images(train_path, log=True):
 
-    angles = set()
-    energies = set()
-    
-    for pt in points:
-        angles.add(pt['theta_cm_out'])
-        energies.add(pt['ke_cm_in'])
-
-    angles = np.array(sorted(angles), dtype=np.float32)
-    energies = np.array(sorted(energies), dtype=np.float32)
-    
-    return angles, energies
-
-def build_sample():
-    pass
-
-def points_to_tensor(samples, angles, energies):
-
-    N = len(samples)
-    A = len(angles)
-    E = len(energies)
-
-    ang_to_idx = {val: i for i, val in enumerate(angles)}
-    en_to_idx  = {val: i for i, val in enumerate(energies)}
-    
-    X = np.full((N, 2, A, E), 0.0, dtype=np.float32)
-    
-    # set mask channel initially 0
-    X[:, 1, :, :] = 0.0
-
-    # fill from points
-    for n, pts in enumerate(samples):
-        for p in pts:
-            a = ang_to_idx.get(p["theta_cm_out"])
-            e = en_to_idx.get(p["ke_cm_in"])
-            if a is None or e is None:
-                # point not on the global axes; skip or handle tolerance matching
-                continue
-            X[n, 0, a, e] = np.float32(p["dsdo"])
-            X[n, 1, a, e] = 1.0
-
-        # If you truly have perfect coverage for every coordinate, you can also do:
-        # X[n, 1, :, :] = 1.0
-
-    return torch.from_numpy(X)
-
-def load_images(train_path):
-    
     with open(train_path, 'r') as f:
         data = json.load(f)
-    
+
     n = len(data)
-    
+    tensors = []
     for i in range(n):
 
-        levels = data[i]['levels'][0]
-        observable_sets = data[i]['observable_sets'][0]
-    
-        if i >= 1:
-            continue
+        points = data[i]['observable_sets'][0]['points']
 
-        # currently only works for a resonance containing one in/out pairing
-        pp_in_index = observable_sets['pp_in_index']
-        pp_out_index = observable_sets['pp_out_index']
-        points = observable_sets['points']
+        df = pd.DataFrame(points)
+        df = df.drop(columns=['ke_cm_in', 'dsdRuth', 'dsdO-dsRuth'])
 
-        angles, energies = build_e_theta_axes(points)
+        if log:
+            df['dsdO'] = np.log10(df['dsdO'])
+
+        grid = df.pivot(index='cn_ex', columns='theta_cm_out', values='dsdO')
+
+        t = torch.tensor(grid.values, dtype=torch.float32)
+        t = t.unsqueeze(0)
+
+        tensors.append(t)
+
+    return torch.stack(tensors, dim=0)
 
 class ResonanceDataset(Dataset):
 
--- model.py
+++ model.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import json
+import seaborn as sns
 from torch.utils.data import DataLoader, Dataset
 from sklearn.metrics import r2_score
 from sklearn.metrics import accuracy_score
@@ -15,7 +16,40 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 import data_loading
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'Using device: {device}')
+if __name__ == "__main__":
 
-data_loading.load_images('data/o16/o16_training.gz')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device: {device}')
+
+    path = 'data/o16/o16_training.gz'
+
+    images = data_loading.get_images(path, log=True) # shape: [1000, 1, 101, 7]
+
+    idx = 0
+    image = images[idx]
+    xs = image[0].numpy()
+    angle = np.arange(xs.shape[1] + 1)
+    energy = np.arange(xs.shape[0] + 1)
+    plt.pcolormesh(angle, energy, xs)
+    plt.show()
+
+    # heatmap = df.pivot(index='cn_ex', columns='theta_cm_out', values='dsdO')
+
+    # plt.figure(figsize=(10, 6))
+
+    # plt.imshow(
+    #     heatmap.values,
+    #     origin='lower',
+    #     aspect='auto',
+    #     cmap='viridis',
+    #     extent=[
+    #         heatmap.columns.min(), heatmap.columns.max(),
+    #         heatmap.index.min(), heatmap.index.max()
+    #     ]
+    # )
+
+    # plt.colorbar(label='Cross Section (dsdO)')
+    # plt.xlabel('Scattering Angle (degrees)')
+    # plt.ylabel('Excitation Energy (MeV)')
+    # plt.title('Differential Cross Section Heatmap')
+    # plt.show()
