45d347ad6746ebf547f159ca089d4f60bb6a72b0
Sam Crowley <samxcrowley@gmail.com>
2025-08-19 20:51:59 +1000

    Added normalise function

--- utils.py
+++ utils.py
@@ -11,6 +11,15 @@ def get_axes(images):
     
     return unique_angles, unique_energies
 
+# normalise a tensor x to [0, 1]
+def normalise(x, min_val=None, max_val=None):
+
+    if min_val is None:
+        min_val = x.min()
+    if max_val is None:
+        max_val = x.max()
+    return (x - min_val) / (max_val - min_val + 1e-8)
+
 def plot_image(images, idx):
 
     image = images[idx]
