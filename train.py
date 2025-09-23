c1ab8c92f11c4f9f1c75a65ee0a94f74248de987
Sam Crowley <samxcrowley@gmail.com>
2025-09-23 15:19:34 +0930

    Cut off mask channel

--- train.py
+++ train.py
@@ -125,6 +125,8 @@ def main():
     if not using_partial_model:
         images = images[:, 0:1, :, :]
 
+    images = images[:, 0:1, :, :]
+
     targets = data_loading.get_targets(training_path)
 
     # if we have defined a smaller subset, cut off the unneeded samples
