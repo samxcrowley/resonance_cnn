b07c90758a28c0ca9bee638ae1607a64b73f5639
Sam Crowley <samxcrowley@gmail.com>
2025-10-02 11:40:55 +1000

    Changed how data loading works to load images and targets simultaneously and account for cropping of target data

--- load_data.py
+++ load_data.py
@@ -6,7 +6,10 @@ import pandas as pd
 import preprocessing
 from torch.utils.data import Dataset
 
-def get_images(train_path, crop_strength, log=True, compressed=True):
+MAX_RESONANCES = 20
+
+# returns images, target_params (energies and gammas), target_mask, target_count
+def get_images_and_targets(train_path, crop_strength, log_cx=True, compressed=True):
 
     if compressed:
         with gzip.open(train_path, 'rb') as f:
@@ -19,62 +22,23 @@ def get_images(train_path, crop_strength, log=True, compressed=True):
 
     n = len(data)
     images = []
+    targets = []
     
     for i in range(n):
 
-        print(f'get_images starting on {i}...')
+        print(f'get_images_and_targets starting on {i}...')
 
         points = data[i]['observable_sets'][0]['points']
         
+        # image data
         E_vals = []
         A_vals = []
         cx_vals = []
 
-        for p in points:
-
-            E_vals.append(p['cn_ex'])
-            A_vals.append(p['theta_cm_out'])
-
-            if log == True:
-                cx = np.log10(p['dsdO'])
-                cx_vals.append(cx)
-            else:
-                cx_vals.append(p['dsdO'])
-
-        for j in range(preprocessing.IMG_DUP):
-            
-            image = preprocessing.place_image_on_grid(E_vals, A_vals, cx_vals)
-
-            if crop_strength > 0.0:
-                print(f'cropping {i}, {j}...')
-                image = preprocessing.crop_image(image, crop_strength)
-
-            images.append(image)
-
-    return torch.stack(images, dim=0)
-
-# get all targets from a training set
-# returns a tensor of shape [n_samples, 2]
-# energy, total width
-def get_targets(train_path, compressed=True):
-
-    if compressed:
-        with gzip.open(train_path, 'rb') as f:
-            json_bytes = f.read()
-            json_str = json_bytes.decode()
-            data = json.loads(json_str)
-    else:
-        with open(train_path, 'r') as f:
-            data = json.load(f)
-
-    n = len(data)
-    tensors = []
-    for i in range(n):
-
+        # target data
         levels = data[i]['levels'][0]
         energy = levels['energy']
         gamma_total = levels['Gamma_total']
-        num_levels = len(data[i]['levels'])
 
         # normalise energy
         points = data[i]['observable_sets'][0]['points']
@@ -84,21 +48,68 @@ def get_targets(train_path, compressed=True):
         Er_unit = (energy - e_min) / max(e_max - e_min, 1e-8)
         Er_unit = float(np.clip(Er_unit, 0.0, 1.0))
 
-        log10_gamma = float(np.log10(gamma_total + 1e-8))
+        # log total gamma
+        log_gamma_total = float(np.log10(gamma_total + 1e-8))
 
-        # duplicate targets to match duplicated (cropped) images
-        for i in range(preprocessing.IMG_DUP):
-            tensors.append(torch.tensor([Er_unit, log10_gamma, num_levels], dtype=torch.float32))
+        # add all image data
+        for p in points:
 
-    return torch.stack(tensors, dim=0)
+            E_vals.append(p['cn_ex'])
+            A_vals.append(p['theta_cm_out'])
+
+            if log_cx:
+                cx_vals.append(np.log10(p['dsdO']))
+            else:
+                cx_vals.append(p['dsdO'])
+
+        # duplicate and process (place on grid and crop) each sample
+        for j in range(preprocessing.IMG_DUP):
+            
+            image = preprocessing.place_image_on_grid(E_vals, A_vals, cx_vals)
+
+            if crop_strength > 0.0:
+
+                print(f'cropping {i}, {j}...')
+
+                image, cropped_E_axis, cropped_A_axis = preprocessing.crop_image(image, crop_strength)
+
+                # find which resonances are in the cropped image
+                levels = data[i]['levels']
+                es = []
+                gs = []
+                for _ in levels:
+                    energy = levels['energy']
+                    gamma_total = levels['gamma_total']
+                    if cropped_E_axis.min() <= energy <= cropped_E_axis.max():
+                        es.append(energy)
+                        gs.append(gamma_total)
+
+                order = torch.argsort(es)
+                e = es[order]
+                g = gs[order]
+
+                # build target tensors
+                count = e.numel()
+                target_params = torch.zeros(MAX_RESONANCES, 2, dtype=torch.float32)
+                target_mask = torch.zeros(MAX_RESONANCES, dtype=torch.bool)
+                target_params[:count, 0] = e[:count]
+                target_params[:count, 1] = g[:count]
+                target_mask[:count] = True
+
+            images.append(image)
+
+    return torch.stack(images, dim=0), \
+            target_params, \
+            target_mask, \
+            torch.tensor(count, dtype=torch.int64)
 
 # input dataset for ResonanceCNN
 class ResonanceDataset(Dataset):
 
-    def __init__(self, images, targets, gradients=True):
+    def __init__(self, images, targets, gradients):
 
-        self.images = images # shape: [N, C=2, E, A]
-        self.targets = targets # shape: [N, 2]
+        self.images = images
+        self.targets = targets
         self.gradients = gradients
 
     def __len__(self):
