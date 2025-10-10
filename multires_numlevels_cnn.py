4d602aad8b1d41f08122b21346c9b904c840a5a8
Sam Crowley <samxcrowley@gmail.com>
2025-10-10 17:29:58 +1100

    Removed small model and changed prediction head to use softplus

--- multires_numlevels_cnn.py
+++ multires_numlevels_cnn.py
@@ -71,49 +71,6 @@ class MultiRes_NumLevels_CNN(nn.Module):
         x = F.relu(self.fc1(feat))
         x = self.dropout(F.relu(self.fc2(x)))
         
-        pred = self.head_num_levels(x).squeeze(-1)
+        pred = F.softplus(self.head_num_levels(x).squeeze(-1)) # >= 0
 
         return pred
-
-class MultiRes_NumLevels_SmallCNN(nn.Module):
-
-    def __init__(self, in_ch=2, base=32, dropout_p=0.1, kernel_size=3, max_levels=5):
-
-        super().__init__()
-        padmode = 'reflect'
-
-        self.conv1 = nn.Conv2d(in_ch, base, kernel_size, padding='same', padding_mode=padmode)
-        self.bn1 = nn.GroupNorm(8, base)
-
-        self.conv2 = nn.Conv2d(base, base * 2, kernel_size, padding='same', padding_mode=padmode)
-        self.bn2 = nn.GroupNorm(8, base * 2)
-
-        self.pool = nn.MaxPool2d(kernel_size=(2,1), stride=(2,1))
-
-        self.dropout = nn.Dropout(dropout_p)
-        self.head = nn.Linear(base*2, max_levels)
-
-    def forward(self, x):
-
-        # x: (N, 2, E, A)
-        img  = x[:, :1]
-        mask = x[:, 1:]  # (N,1,E,A)
-
-        h = F.relu(self.bn1(self.conv1(torch.cat([img, mask], dim=1))))
-        h = self.pool(h)
-
-        h = F.relu(self.bn2(self.conv2(h)))
-        h = self.pool(h)
-
-        # --- masked GAP ---
-        # Resize mask once to match h's spatial size
-        mask_ds = F.interpolate(mask, size=h.shape[-2:], mode='nearest')
-        mask_b  = (mask_ds > 0).float()
-
-        denom = mask_b.sum(dim=(2,3)).clamp_min(1e-6)   # (N,1)
-        num   = (h * mask_b).sum(dim=(2,3))             # (N,C)
-        feat  = num / denom                              # broadcast → (N,C)
-
-        feat  = self.dropout(feat)
-        logits = self.head(feat)                         # (N, max_levels)
-        return logits
